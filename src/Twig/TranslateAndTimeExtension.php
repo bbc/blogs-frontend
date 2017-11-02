74ffefc416ad3db80d452ef96ee08988be96d4e0
Michael Gwynne <votemike@users.noreply.github.com>
2017-11-02 12:13:28 +0000

    BLOGS-1258 Translations (#1)

---
+++ src/Twig/TranslateAndTimeExtension.php
@@ -0,0 +1,42 @@
+<?php
+declare(strict_types = 1);
+
+namespace App\Twig;
+
+use App\Translate\TranslatableTrait;
+use App\Translate\TranslateProvider;
+use Twig_Extension;
+use Twig_Function;
+
+/**
+ * The local time functions make use of Translate fairly heavily.
+ * Hence grouping local time and translation together.
+ */
+class TranslateAndTimeExtension extends Twig_Extension
+{
+    use TranslatableTrait;
+
+    public function __construct(TranslateProvider $translateProvider)
+    {
+        $this->translateProvider = $translateProvider;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getFunctions()
+    {
+        return [
+            new Twig_Function('tr', [$this, 'trWrapper']),
+        ];
+    }
+
+    public function trWrapper(
+        string $key,
+        $substitutions = [],
+        $numPlurals = null,
+        ?string $domain = null
+    ): string {
+        return $this->tr($key, $substitutions, $numPlurals, $domain);
+    }
+}
