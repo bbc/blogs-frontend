b484c71e0a56de85e227609c250f187df313ae39
Ben Scott <ben.scott@bbc.co.uk>
2017-11-10 13:14:59 +0000

    Check for dotenv incase we're running without dev dependencies

--- web/index.php
+++ web/index.php
@@ -8,7 +8,7 @@ use Symfony\Component\HttpFoundation\Request;
 require __DIR__.'/../vendor/autoload.php';
 
 // The check is to ensure we don't use .env in production
-if (!isset($_SERVER['APP_ENV'])) {
+if (class_exists(Dotenv::class) && !isset($_SERVER['APP_ENV'])) {
     (new Dotenv())->load(__DIR__.'/../.env');
 }
 
