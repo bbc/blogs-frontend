6f56be98c8f45ef6028c2850c6c226c2678d17c1
Michael Gwynne <michael.gwynne@bbc.co.uk>
2017-11-20 12:46:39 +0000

    BLOGS-1282 Adding About Module

--- src/Ds/PresenterFactory.php
+++ src/Ds/PresenterFactory.php
@@ -1,15 +1,28 @@
 <?php
 declare(strict_types = 1);
+
 namespace App\Ds;
 
+use App\BlogsService\Domain\Blog;
 use App\BlogsService\Domain\Image;
 use App\Ds\Molecule\Image\ImagePresenter;
+use App\Ds\Organism\Module\AboutPresenter;
 
 /**
  * Ds Factory Class for creating presenters.
  */
 class PresenterFactory
 {
+    public function aboutModulePresenter(
+        Blog $blog,
+        array $options = []
+    ): AboutPresenter {
+        return new AboutPresenter(
+            $blog,
+            $options
+        );
+    }
+
     public function imagePresenter(
         Image $image,
         int $defaultWidth,
