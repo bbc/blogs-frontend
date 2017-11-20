b22a3b46591090e2647d0534f2105fe94ff98c5f
Michael Gwynne <michael.gwynne@bbc.co.uk>
2017-11-20 12:46:39 +0000

    BLOGS-1282 Adding About Module

--- src/Ds/PresenterFactory.php
+++ src/Ds/PresenterFactory.php
@@ -7,6 +7,7 @@ use App\BlogsService\Domain\Blog;
 use App\BlogsService\Domain\Image;
 use App\Ds\Module\AboutPresenter;
 use App\Ds\Molecule\Image\ImagePresenter;
+use App\Ds\Organism\Module\AboutPresenter;
 
 /**
  * Ds Factory Class for creating presenters.
