8c3fb745d9926a752b8cafdf1f42c5a81a670f50
Matt Greenham <matt.greenham@bbc.co.uk>
2017-11-21 18:12:01 +0000

    Change service test folders

--- tests/BlogsService/Service/BlogService/GetAllBlogsTest.php
+++ tests/BlogsService/Service/BlogService/GetAllBlogsTest.php
@@ -3,41 +3,24 @@ declare(strict_types = 1);
 
 namespace Tests\App\BlogsService\Service\BlogService;
 
-use App\BlogsService\Infrastructure\Cache\CacheInterface;
-use App\BlogsService\Infrastructure\IsiteFeedResponseHandler;
 use App\BlogsService\Infrastructure\IsiteResult;
 use App\BlogsService\Repository\BlogRepository;
 use App\BlogsService\Service\BlogService;
 use GuzzleHttp\Psr7\Response;
-use PHPUnit\Framework\TestCase;
 use PHPUnit_Framework_MockObject_MockObject;
+use Tests\App\BlogsService\Service\ServiceTest;
 
-class GetAllBlogsTest extends TestCase
+class GetAllBlogsTest extends ServiceTest
 {
     /** @var BlogRepository | PHPUnit_Framework_MockObject_MockObject */
     private $mockBlogRepository;
-
-    /** @var  IsiteFeedResponseHandler | PHPUnit_Framework_MockObject_MockObject */
-    private $mockIsiteFeedResponseHandler;
-
-    /** @var CacheInterface | PHPUnit_Framework_MockObject_MockObject */
-    private $mockCache;
 
     public function setUp()
     {
         $this->mockBlogRepository = $this->createMock(BlogRepository::class);
 
-        $this->mockIsiteFeedResponseHandler = $this->createMock(IsiteFeedResponseHandler::class);
-
-        $this->mockCache = $this->createMock(CacheInterface::class);
-        $this->mockCache
-            ->expects($this->once())
-            ->method('getOrSet')
-            ->will($this->returnCallback(
-                function (string $key, $ttl, callable $function, array $arguments = []) {
-                    return $function(...$arguments);
-                }
-            ));
+        $this->setUpMockResponseHandler();
+        $this->setUpMockCache();
     }
 
     /**
