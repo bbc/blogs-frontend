a9fea47d2e651f6641d22ff61c1cc4779d8e13de
Michael Gwynne <michael.gwynne@bbc.co.uk>
2017-11-24 09:44:52 +0000

    Adding all tags page

---
+++ src/Controller/TagIndexController.php
@@ -0,0 +1,26 @@
+<?php
+declare(strict_types = 1);
+
+namespace App\Controller;
+
+use App\BlogsService\Domain\Blog;
+use App\BlogsService\Service\TagService;
+use App\Ds\Molecule\Paginator\PaginatorPresenter;
+use Symfony\Component\HttpFoundation\Request;
+
+class TagIndexController extends BlogsBaseController
+{
+    public function __invoke(Request $request, Blog $blog, TagService $tagService)
+    {
+        $this->setBlog($blog);
+
+        $tagsResult = $tagService->getTagsByBlog($blog, (int) $request->query->get('page', 1), 10);
+
+        $paginator = null;
+        if ($tagsResult->getTotal() > $tagsResult->getPageSize()) {
+            $paginator = new PaginatorPresenter($tagsResult->getPage(), $tagsResult->getPageSize(), $tagsResult->getTotal());
+        }
+
+        return $this->renderWithChrome('tag/index.html.twig', ['tagResult' => $tagsResult, 'paginatorPresenter' => $paginator]);
+    }
+}
