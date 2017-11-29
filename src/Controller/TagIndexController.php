6cf15c4811f3cb70e156393a6390522d1e36ce6e
Michael Gwynne <votemike@users.noreply.github.com>
2017-11-29 09:42:21 +0000

    BLOGS-1289 all tags page (#46)

---
+++ src/Controller/TagIndexController.php
@@ -0,0 +1,31 @@
+<?php
+declare(strict_types = 1);
+
+namespace App\Controller;
+
+use App\BlogsService\Domain\Blog;
+use App\BlogsService\Service\TagService;
+use App\Ds\Molecule\Paginator\PaginatorPresenter;
+use Symfony\Component\HttpFoundation\Request;
+
+class TagIndexController extends BlogsBaseController
+{
+    public function __invoke(Request $request, Blog $blog, TagService $tagService)
+    {
+        $this->setBlog($blog);
+
+        $page = (int) $request->query->get('page', 1);
+        if ($page < 1) {
+            $page = 1;
+        }
+
+        $tagsResult = $tagService->getTagsByBlog($blog, $page, 10);
+
+        $paginator = null;
+        if ($tagsResult->getTotal() > $tagsResult->getPageSize()) {
+            $paginator = new PaginatorPresenter($tagsResult->getPage(), $tagsResult->getPageSize(), $tagsResult->getTotal());
+        }
+
+        return $this->renderWithChrome('tag/index.html.twig', ['tagResult' => $tagsResult, 'paginatorPresenter' => $paginator]);
+    }
+}
