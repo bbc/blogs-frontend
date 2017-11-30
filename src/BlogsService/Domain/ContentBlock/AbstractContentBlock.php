754bdc7e8f2b607c3656690194e3ef30eb939006
Matt Greenham <matt.greenham@bbc.co.uk>
2017-11-30 11:43:16 +0000

    Pr Comments

--- src/BlogsService/Domain/ContentBlock/AbstractContentBlock.php
+++ src/BlogsService/Domain/ContentBlock/AbstractContentBlock.php
@@ -7,8 +7,3 @@ abstract class AbstractContentBlock
 {
     abstract public function getCharacterCount(): int;
 }
-
-
-// This is a default value allocated to content blocks such as clips
-// for the purposes of post truncation
-// TODO check this when implementing post truncation
