2c3c58ac04cc23408a69686c0f396481e7fc4dec
Felipe Araujo <felipe.peixotodearaujo@bbc.co.uk>
2017-12-14 13:02:30 +0000

    Catching exceptions with error message.

--- src/Service/CommentsService.php
+++ src/Service/CommentsService.php
@@ -8,6 +8,7 @@ use App\BlogsService\Domain\Post;
 use BBC\ProgrammesMorphLibrary\Entity\MorphView;
 use BBC\ProgrammesMorphLibrary\Exception\MorphErrorException;
 use BBC\ProgrammesMorphLibrary\MorphClient;
+use Psr\Log\LoggerInterface;
 
 class CommentsService
 {
@@ -20,11 +21,15 @@ class CommentsService
     /** @var MorphClient */
     private $client;
 
-    public function __construct(MorphClient $client, string $apiKey, string $env)
+    /** @var LoggerInterface */
+    private $logger;
+
+    public function __construct(LoggerInterface $logger, MorphClient $client, string $apiKey, string $env)
     {
         $this->apiKey = $apiKey;
         $this->env = $env;
         $this->client = $client;
+        $this->logger = $logger;
     }
 
     public function queuePostComments(Blog $blog, Post $post): void
@@ -43,17 +48,28 @@ class CommentsService
 
     public function getPostComments(Blog $blog, Post $post): ?MorphView
     {
-        return $this->client->getView(
-            'bbc-morph-comments-view',
-            'comments-module',
-            [
-                'apiKey' => $this->apiKey,
-                'mode' => 'embedded',
-                'idctaEnv' => $this->env,
-                'forumId' => 'blogs_' . $blog->getId() . $post->getForumId(),
-            ],
-            [],
-            'There was an error when fetching the comments. Please try again in a few moments.'
-        );
+        try {
+            return $this->client->getView(
+                'bbc-morph-comments-view',
+                'comments-module',
+                [
+                    'apiKey' => $this->apiKey,
+                    'mode' => 'embedded',
+                    'idctaEnv' => $this->env,
+                    'forumId' => 'blogs_' . $blog->getId() . $post->getForumId(),
+                ],
+                [],
+                'There was an error when fetching the comments. Please try again in a few moments.'
+            );
+        }
+        catch (MorphErrorException $e) {
+            $this->logger->error($e->getMessage());
+            return new MorphView(
+                'comments-module',
+                [],
+                'There was an error when fetching the comments. Please try again in a few moments.',
+                []
+            );
+        }
     }
 }
