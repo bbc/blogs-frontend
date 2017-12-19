2c2faf0d1dc60644dae78e11c2a4a79c0dc36e8e
Matt Greenham <matt.greenham@bbc.co.uk>
2017-12-19 17:40:55 +0000

    Adding tests

--- tests/BlogsService/Mapper/IsiteToDomain/BlogMapperTest.php
+++ tests/BlogsService/Mapper/IsiteToDomain/BlogMapperTest.php
@@ -5,29 +5,25 @@ namespace Tests\App\BlogsService\Mapper\IsiteToDomain;
 use App\BlogsService\Domain\Blog;
 use App\BlogsService\Infrastructure\MapperFactory;
 use App\BlogsService\Mapper\IsiteToDomain\BlogMapper;
+use App\BlogsService\Mapper\IsiteToDomain\PostMapper;
 use PHPUnit\Framework\TestCase;
 use SimpleXMLElement;
 
 class BlogMapperTest extends TestCase
 {
-    private $isiteObject;
-
-    public function setUp()
+    public function testGetsDomainModel()
     {
         $type = 'blogsmetadata.xml';
         $basePath = __DIR__ . '/../../../mock_data/';
 
         $xml = file_get_contents($basePath . $type);
-        $this->isiteObject = new SimpleXMLElement($xml);
-    }
+        $isiteObject = new SimpleXMLElement($xml);
 
-    public function testGetsDomainModel()
-    {
         $mapperFactory = $this->createMock(MapperFactory::class);
         $blogMapper = new BlogMapper($mapperFactory);
 
         /** @var Blog $domainModel */
-        $domainModel = $blogMapper->getDomainModel($this->isiteObject);
+        $domainModel = $blogMapper->getDomainModel($isiteObject);
 
         $this->assertEquals('aboutthebbc', $domainModel->getId());
         $this->assertEquals('About the BBC', $domainModel->getName());
@@ -41,5 +37,26 @@ class BlogMapperTest extends TestCase
             'This blog explains what the BBC does and how it works. We link to some other blogs and online spaces inside and outside the corporation. The blog is edited by Jon Jacob.',
             $domainModel->getDescription()
         );
+    }
+
+    public function testIgnoresInvalidFeaturedPost()
+    {
+        $type = 'blogsmetadata_invalidfeatured.xml';
+        $basePath = __DIR__ . '/../../../mock_data/';
+
+        $xml = file_get_contents($basePath . $type);
+        $isiteObject = new SimpleXMLElement($xml);
+
+        $mapperFactory = $this->createMock(MapperFactory::class);
+        $postMapper = new PostMapper($mapperFactory);
+        $mapperFactory->method('createPostMapper')->willReturn($postMapper);
+        $blogMapper = new BlogMapper($mapperFactory);
+
+        /** @var Blog $domainModel */
+        $blog = $blogMapper->getDomainModel($isiteObject);
+
+        $featuredPost = $blog->getFeaturedPost();
+
+        $this->assertNull($featuredPost);
     }
 }
