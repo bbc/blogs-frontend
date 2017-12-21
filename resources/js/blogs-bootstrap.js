872e922536294e7471d923ee18a21029ef364f52
Michael Gwynne <michael.gwynne@bbc.co.uk>
2017-12-21 14:08:21 +0000

    There are a load of never used options etc.... tidying...

--- resources/js/blogs-bootstrap.js
+++ resources/js/blogs-bootstrap.js
@@ -1,11 +1,7 @@
 define(['istats-tracking', 'respimg', 'lazysizes'], function (IstatsTracking) {
-    function init() {
-        var tracking = new IstatsTracking();
-        tracking.init();
-    }
-
     // cut the mustard
     if ('querySelector' in document && 'addEventListener' in window) {
-        init();
+        var tracking = new IstatsTracking();
+        tracking.init();
     }
 });
