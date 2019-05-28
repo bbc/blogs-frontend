7b0a2a7a2618c46ed1567a63a709aab0442c1c6f
Sam Fry <sam.fry@bbc.co.uk>
2019-05-28 09:04:11 +0100

    Fix typo

--- gulpfile.js
+++ gulpfile.js
@@ -28,7 +28,7 @@ gulp.task('js:clean', function () {
     return del([staticPathDist + '/js']);
 });
 
-gulp.task('js', gulp.series('js:clean' , function () {
+gulp.task('js', gulp.series('js:clean', function () {
     const modulesToOptimize = [
         staticPathSrc + '/js/**/blogs-bootstrap.js',
         staticPathSrc + '/js/**/third-party.js',
